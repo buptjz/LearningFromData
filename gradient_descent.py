dfb0658ef96afffc19f442bc4d27e6c87d030798
buptjz <buptjz@gmail.com>
2014-11-03 09:40:47 +0800

    完成梯度下降

--- gradient_descent.py
+++ gradient_descent.py
@@ -23,6 +23,11 @@ w(t + 1) = w(t) − η ∇Ein(w(t))
 from math import e
 
 
+def e_func(u, v):
+    x = u * pow(e, v) - 2 * v * pow(e, -u)
+    return x * x
+
+
 def partial_u(u, v):
     """计算u偏导"""
     fir = 2 * (u * pow(e, v) - 2 * v * pow(e, -u))
@@ -37,3 +42,15 @@ def partial_v(u, v):
     sec = (u * pow(e, v) - 2 * pow(e, -u))
 
     return fir * sec
+
+glb_u, glb_v = 1.0, 1.0
+eta = 0.1
+goal = pow(10, -14)
+cnt = 15
+
+while cnt > 0:
+    cnt -= 1
+    glb_u -= eta * partial_u(glb_u, glb_v)
+    glb_v -= eta * partial_v(glb_u, glb_v)
+
+print e_func(glb_u, glb_v)
