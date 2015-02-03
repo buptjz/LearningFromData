fc1ee94bfe558ccf56bad8f1311a223b27100e9e
buptjz <buptjz@gmail.com>
2015-02-03 11:54:56 +0800

    调整了下结构

--- MachineLearningTechniques/hw3/decision_tree.py
+++ MachineLearningTechniques/hw3/decision_tree.py
@@ -2,29 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 
+
 class DT_Node:
     '''Decision Tree Node'''
     def __init__(self, axis=None, thres=None):
-        #axis is the dimension of this branching
-        #theta is the threshold in that dimension
-        self.left = None
-        self.right = None
-        self.axis = axis
-        self.thres = thres
+        self.left = None#riht child
+        self.right = None#left child
+        self.axis = axis#axis is the dimension of this branching
+        self.thres = thres#thres is the threshold in that dimension
+        self.label = None#class label which determines if it's a leave node
 
-        #class label which determines if it's a leave node
-        self.label = None
 
 def plot_sign(X, Y):
     plt.plot(X[Y > 0, 0], X[Y > 0, 1], 'ro')
     plt.plot(X[Y < 0, 0], X[Y < 0, 1], 'go')
 
-# load a file and get [X Y]
+
 def load_file(file_name):
     lines = open(file_name, 'r').readlines()
-
     data = np.array([np.fromstring(line, dtype=float, sep=' ') for line in lines])
-
     X = data[:, :-1]
     Y = data[:, -1].astype(np.int)
     return X, Y
@@ -49,19 +45,16 @@ def gini_index(Y):
 def decide_branch(X, Y):
     '''Decide axis and theta according to the training datas'''
     D, N = X.shape[1], X.shape[0] #num of features,examples
-
     # init with not branch at all
     min_err = N * gini_index(Y)
     best_axis = 0
     best_thres = -np.inf
-
     #consider different features
     for d in range(D):
         # sort the rows by feature (column) d
         sortidx = X[:, d].argsort()
         X_sorted = X[sortidx]
         Y_sorted = Y[sortidx]
-
         #consider different branching positions (totally N-1)
         for i in range(N-1):
             #split between i and i+1,left #i+1 examples and right #N-i-1 examples
@@ -100,7 +93,6 @@ def build_decision_tee(X, Y):
     Building decision tree recursively
     datas: a list of datas
     '''
-
     assert X.shape[0] != 0
 
     #base case 1: all yn the same: impurity = 0,gt(x) = yn
@@ -108,13 +100,11 @@ def build_decision_tee(X, Y):
         leave_node = DT_Node()
         leave_node.label = Y[0]
         return leave_node
-
     #all xn the same: no decision stumps, gt(x) = majority of yn
     elif all_same_rows(X) :
         leave_node = DT_Node()
         leave_node.label = majority_item(Y)
         return leave_node
-
     #common case
     else:
         axis, thres = decide_branch(X, Y)
@@ -193,5 +183,3 @@ if __name__ == '__main__':
     print '----------------------------------------'
     print 'Eout (evaluated with 0/1 error):'
     print  float(num_wrong) / N
-
-    
