b8397beb8e55c03bcd98cbddfb0a05b34d1b8d22
buptjz <buptjz@gmail.com>
2015-02-03 23:27:03 +0800

    优化gini_index的性能瓶颈

--- MachineLearningTechniques/hw3/decision_tree.py
+++ MachineLearningTechniques/hw3/decision_tree.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
-
+from collections import Counter,defaultdict
 
 class DT_Node:
     '''Decision Tree Node'''
@@ -35,10 +34,11 @@ def majority_item(Y):
 
 def gini_index(Y):
     '''Compute gini index, consider all k class'''
-    Y_list = list(Y)
-    N = len(Y_list)
-    freqs = [Y_list.count(c) for c in set(Y_list)]
-    error = 1 - sum([((f*1.0) / N)**2 for f in freqs])
+    #since binary classification, we simplify the implementation
+    #count_nonzero is much faster than sum!
+    pos = np.count_nonzero(Y == 1)
+    neg = np.count_nonzero(Y == -1)
+    error = 1 - float(pos**2 + neg**2) / ((pos + neg)**2)
     return error
 
 
@@ -88,7 +88,7 @@ def all_same_rows(X):
     return True
 
 
-def build_decision_tee(X, Y):
+def build_decision_tree(X, Y):
     '''
     Building decision tree recursively
     datas: a list of datas
@@ -111,8 +111,8 @@ def build_decision_tee(X, Y):
         node = DT_Node(axis, thres)
         left_X, left_Y = datas_below(X, Y, axis, thres)
         right_X, right_Y = datas_above(X, Y, axis, thres)
-        node.left = build_decision_tee(left_X, left_Y)
-        node.right = build_decision_tee(right_X, right_Y)
+        node.left = build_decision_tree(left_X, left_Y)
+        node.right = build_decision_tree(right_X, right_Y)
         return node
 
 
@@ -142,13 +142,29 @@ def print_tree(dtree, spaces):
     print_tree(dtree.right, spaces + 2)
 
 
+def build_random_forest(T, Nchoose, X_train, Y_train):
+    '''
+    Random forest (RF) = bagging + fully-grown C&RT decision tree
+    T: num of decision trees
+    Nchoose : choose how many exapmles per loop?
+    Return a list of decision trees
+    '''
+    dtrees = []
+    for t in range(T):
+        randidx = (100 * np.random.random(size=Nchoose)).astype(int)
+        X_train_chosen = X_train[randidx, :]
+        Y_train_chosen = Y_train[randidx]
+        dt = build_decision_tree(X_train_chosen,Y_train_chosen)
+        dtrees.append(dt)
+    return dtrees
+
 num_nodes = 0
-if __name__ == '__main__':
+def main():
     train_file = "hw3_train.dat"
     test_file = "hw3_test.dat"
     X_train, Y_train = load_file(train_file)
     N = X_train.shape[0]# num of training examples
-    dtree = build_decision_tee(X_train, Y_train)
+    dtree = build_decision_tree(X_train, Y_train)
 
     #Q13 : How many internal nodes (branching functions) are there in the resulting tree G?
     print_tree(dtree, 0)
@@ -165,33 +181,83 @@ if __name__ == '__main__':
     print '         Homework 3 Question 14         '
     print '----------------------------------------'
     print 'Ein (evaluated with 0/1 error):'
-    print  float(num_wrong) / N
+    print float(num_wrong) / N
 
     #Q15 : Which of the following is closest to the Eout (evaluated with 0/1 error) of the tree?
     X_test, Y_test = load_file(test_file)
     Predicts_test = [predict_y(dtree, x)for x in X_test]
-    N = X_test.shape[0]
-    num_wrong = N - sum(Predicts_test == Y_test)
+    num_wrong = sum(Predicts_test != Y_test)
     print '----------------------------------------'
     print '         Homework 3 Question 15         '
     print '----------------------------------------'
     print 'Eout (evaluated with 0/1 error):'
-    print  float(num_wrong) / N
+    print  float(num_wrong) / X_test.shape[0]
 
-    #Show the decision boundary and training points
-    start = 0
-    end = 1
-    npoints = 500
-    xrange = np.linspace(start, end, npoints)
-    yrange = np.linspace(start, end, npoints)
-    xgrid, ygrid = np.meshgrid(xrange,yrange)
+    # #[SHOW] Plot the decision boundary and training points
+    # start = 0
+    # end = 1
+    # npoints = 500
+    # xrange = np.linspace(start, end, npoints)
+    # yrange = np.linspace(start, end, npoints)
+    # xgrid, ygrid = np.meshgrid(xrange,yrange)
+    #
+    # X = np.array([xgrid.reshape(npoints * npoints), ygrid.reshape(npoints * npoints)]).T
+    # labels = [predict_y(dtree,x) for x in X]
+    # zgrid = np.array(labels).reshape(npoints, npoints)
+    # plt.figure()
+    # plt.title('Decision Tree')
+    # plot_sign(X_train,Y_train)
+    # plt.pcolor(xgrid, ygrid, zgrid)
+    # plt.show()
+    # '''We can see overfit obviously!!'''
 
-    X = np.array([xgrid.reshape(npoints * npoints), ygrid.reshape(npoints * npoints)]).T
-    labels = [predict_y(dtree,x) for x in X]
-    zgrid = np.array(labels).reshape(npoints, npoints)
-    plt.figure()
-    plt.title('Decision Tree')
-    plot_sign(X_train,Y_train)
-    plt.pcolor(xgrid, ygrid, zgrid)
-    plt.show()
-    '''We can see overfit obviously!!'''
+
+    # Q16,17,18:Produce Random Forest with bagging. N'= N,T=300, repeat the experiment for 100 times
+    REPEAT = 100
+    T = 300
+    random_forests_list = [build_random_forest(T, N, X_train, Y_train) for i in range(REPEAT)]
+
+    print '----------------------------------------'
+    print '         Homework 3 Question 16         '
+    print '----------------------------------------'
+    print 'Average Ein(gt) for all the 30000 trees that you have generated'
+    eins_30000 = []
+    for forest in random_forests_list:
+        for tree in forest:
+            Predicts_train = [predict_y(tree, x) for x in X_train]
+            nwrong = np.count_nonzero(Predicts_train != Y_train)
+            eins_30000.append(float(nwrong) / N)
+    print np.average(eins_30000)
+
+    print '----------------------------------------'
+    print '         Homework 3 Question 17,18      '
+    print '----------------------------------------'
+
+    rf_eins_100, rf_eouts_100 = [], []#keep track of the eins and eouts
+    ntrain, ntest = X_train.shape[0], X_test.shape[0]
+    for forest in random_forests_list:
+        Predicts_train, Predicts_test = [], []
+        for x in X_train:
+            tmp_predict = majority_item([predict_y(tree, x) for tree in forest])
+            Predicts_train.append(tmp_predict)
+        nwrong = np.count_nonzero(Predicts_train != Y_train)
+        rf_eins_100.append(float(nwrong) / ntrain)
+
+        for x in X_test:
+            tmp_predict = majority_item([predict_y(tree, x) for tree in forest])
+            Predicts_test.append(tmp_predict)
+        nwrong = np.count_nonzero(Predicts_test != Y_test)
+        rf_eouts_100.append(float(nwrong) / ntest)
+    print 'Which of the following is true about the average Ein(GRF)?'
+    print np.average(rf_eins_100)
+    print 'Which of the following is true about the average Eout(GRF)?'
+    print np.average(rf_eouts_100)
+
+
+if __name__ == '__main__':
+    # import cProfile
+    # cProfile.run("main()", 'result')
+    # import pstats
+    # p = pstats.Stats("result")
+    # p.strip_dirs().sort_stats("cumulative").print_stats()
+    main()
